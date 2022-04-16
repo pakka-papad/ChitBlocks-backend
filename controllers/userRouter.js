aeb843bc3628b31fb5cb5df1e0ace175f84233f8
Shreshtha13 <shreshtha_m@cs.iitr.ac.in>
2022-04-16 17:27:53 +0530

    changes

--- controllers/userRouter.js
+++ controllers/userRouter.js
@@ -1,25 +1,45 @@
 const userRouter = require('express').Router()
 const User = require('../models/users')
 
+let userArray = []
+
 userRouter.get('/', (request, response) => {
-  User.find({}).then(notes => {
-    response.json(notes)
+  User.find({}).then(users => {
+    response.json(users)
   })
 })
 
 userRouter.post('/', (request, response, next) => {
   const body = request.body
+  var isPresent = false
+  User.find({}).then(users => {
+    userArray = users
+    userArray.map(item => {
+      console.log('item.userAcc', item.userAcc);
+      console.log('body.userAcc', body.userAcc);
+      console.log(item.userAcc == body.userAcc);
 
-  const user = new User({
-    userAcc: body.userAcc,
-    groupId: body.groupId
-  })
-
-  user.save()
-    .then(savedUser => {
-      response.json(savedUser)
+      if (item.userAcc == body.userAcc) { isPresent = true; }
     })
-    .catch(error => next(error))
+  }).then(
+    () => {
+      console.log('isPresent', isPresent)
+      if (!isPresent) {
+        const user = new User({
+          userAcc: body.userAcc,
+          groupId: body.groupId,
+          scheme: body.scheme
+        })
+
+        user.save()
+          .then(savedUser => {
+            response.json(savedUser)
+          })
+          .catch(error => next(error))
+      }
+    }
+  )
+
 })
 
 
